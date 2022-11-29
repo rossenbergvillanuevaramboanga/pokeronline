50886a072d4511df0a19f45711ba4967d765d968
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-29 10:50:14 +0100

    first commit

---
+++ src/main/java/it/prova/pokeronline/PokeronlineApplication.java
@@ -0,0 +1,13 @@
+package it.prova.pokeronline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeronlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeronlineApplication.class, args);
+	}
+
+}
